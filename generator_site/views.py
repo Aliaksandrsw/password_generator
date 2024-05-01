4ccabfb29dcd00fecb7e008d2f4565c3894ff09c
Aliaksandrsw <swordswird@proton.me>
2024-05-01 15:26:03 +0300

    Исправление отправки письма

--- generator_site/views.py
+++ generator_site/views.py
@@ -1,12 +1,7 @@
-import sys
-
 from django.contrib import messages
 from django.core.mail import send_mail
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
-
-import password_generator
 from .forms import PasswordGeneratorForm
 import string
 import random
@@ -32,6 +27,7 @@ class PasswordGeneratorView(FormView):
             characters += string.punctuation
 
         password = ''.join(random.choice(characters) for _ in range(length))
+        self.request.session['generated_password'] = password
         messages.success(self.request, f'Сгенерированный пароль: {password}')
         return super().form_valid(form)
 
@@ -39,30 +35,38 @@ class PasswordGeneratorView(FormView):
 class SendPasswordEmailView(FormView):
     template_name = 'generator/send_password_email.html'
     form_class = PasswordGeneratorForm
-    success_url = reverse_lazy('password_gen')
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields = {'email': form.fields['email']}
+        return form
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        password = self.request.session.get('generated_password')
+        if password:
+            context['password'] = password
+        return context
 
     def form_valid(self, form):
         password = self.request.POST.get('password')
-        print(password)
         email = form.cleaned_data.get('email')
 
         if email:
-            print(f'Email: {email}')
-            print(f'Password in POST data: {password}')
             try:
                 send_mail(
-                    'Your Generated Password',
-                    f'Your generated password is: {password}',
-                    'noreply@example.com',
+                    f'Ваш сгенерированный пароль: {password}',
+                    'example@ya.com',
                     [email],
                     fail_silently=False,
                 )
 
-                messages.success(self.request, 'Password sent to your email.')
+                messages.success(self.request, 'Пароль отпрвлен на вашу почту')
+                del self.request.session['generated_password']
             except Exception as e:
                 messages.error(self.request, 'Failed to send password to your email.')
 
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('password_generator')
+        return reverse_lazy('password_gen')
